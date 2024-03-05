1cd67a07ba757e3348512b25203a9277ef9c7954
박세우(Sewoo Park)/전시플랫폼개발팀/11ST <sewoo@sk.com>
2024-03-05 16:27:17 +0900

    main | 박세우 | 기본 구동되게 수정 #2

--- domain/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    java
-    id("org.springframework.boot") version "3.2.3"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-}
-
-dependencies {
-    // 자바 프로젝트에 필요한 의존성을 여기에 추가하세요.
-}
