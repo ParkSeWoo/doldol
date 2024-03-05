64ff7f031e9fe60bce0917ab713107404adc2e1c
박세우(Sewoo Park)/전시플랫폼개발팀/11ST <sewoo@sk.com>
2024-03-05 12:16:20 +0900

    main | 박세우 | init 최초 commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.3"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("org.graalvm.buildtools.native") version "0.9.28"
+}
+
+group = "io.sw"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/io/sw/doldol/DoldolApplication.java
@@ -0,0 +1,13 @@
+package io.sw.doldol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoldolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoldolApplication.class, args);
+	}
+
+}
